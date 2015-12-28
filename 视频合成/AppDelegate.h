78b2789ce4d4f6b5cd2897950015f4aa5e236e0a
王明朗 <minglang.wang@hjwang.com>
2015-12-28 16:02:50 +0800

    Initial Commit

---
+++ 视频合成/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  视频合成
+//
+//  Created by WangMinglang on 15/12/28.
+//  Copyright © 2015年 好价. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 视频合成/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  视频合成
+//
+//  Created by WangMinglang on 15/12/28.
+//  Copyright © 2015年 好价. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
